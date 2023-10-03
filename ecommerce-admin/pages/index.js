c2b958ed0d4a7092a6b49b6e4a90fb5df0350cdb
Dany-Duran NZIGAMASABO <63962014+DANY-DURAND@users.noreply.github.com>
2023-10-03 11:18:32 +0800

    Add authentification

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div>
-        <div className={'bg-red-500 text-white'}>test</div>
+      <div className="text-center w-full">
+        <button className="bg-white p-2  px-4 rounded-lg"> Login with Google</button>
       </div>
     </div>
   )
